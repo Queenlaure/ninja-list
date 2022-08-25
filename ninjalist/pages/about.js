967b5be83cf947cef0d5d8eefe05310e115a975e
Nfongeh Queen <queennfongeh@gmail.com>
2022-08-25 09:58:44 -0400

    initial

---
+++ ninjalist/pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head> 
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, eum architecto vero dolores necessitatibus a esse pariatur porro perspiciatis temporibus vitae ea, suscipit nobis. Eius, sunt animi. Est, libero excepturi!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, eum architecto vero dolores necessitatibus a esse pariatur porro perspiciatis temporibus vitae ea, suscipit nobis. Eius, sunt animi. Est, libero excepturi!</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
